490cb5c35d1b11d1c3f03efe776ab8df61e65e75
Lukáš Voborský <vsky279@users.noreply.github.com>
2022-10-02 11:25:41 +0200

    Heltec LoRa 32 v2 and v3 boards

--- software/firmware/source/SoftRF/src/platform/iomap/Heltec_Tracker.h
+++ software/firmware/source/SoftRF/src/platform/iomap/Heltec_Tracker.h
@@ -1,10 +1,22 @@
 
 // GNSS module
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+
 #define SOC_GPIO_PIN_HELTRK_GNSS_RX     33
 #define SOC_GPIO_PIN_HELTRK_GNSS_TX     34
+#define SOC_GPIO_PIN_HELTRK_GNSS_RST    20 // 35 is LED
+#define SOC_GPIO_PIN_HELTRK_GNSS_PPS    19 // 36 is Vext_Ctrl
+#define SOC_GPIO_PIN_HELTRK_GNSS_EN     23 /* active LOW */
+
+#else 
+
+#define SOC_GPIO_PIN_HELTRK_GNSS_RX     33
+#define SOC_GPIO_PIN_HELTRK_GNSS_TX     17 // 34 is invalid TX for ESP32
 #define SOC_GPIO_PIN_HELTRK_GNSS_RST    35
 #define SOC_GPIO_PIN_HELTRK_GNSS_PPS    36
-#define SOC_GPIO_PIN_HELTRK_GNSS_EN     37 /* active LOW */
+#define SOC_GPIO_PIN_HELTRK_GNSS_EN     23 /* active LOW */
+
+#endif
 
 // SX1262
 #define SOC_GPIO_PIN_HELTRK_MOSI        10
@@ -26,12 +38,34 @@
 #define SOC_GPIO_PIN_HELTRK_TFT_BL_V05  21 /* V1.1 PCB marking */
 #define SOC_GPIO_PIN_HELTRK_TFT_EN      46 /* active LOW */
 
+// OLED
+// Hardware pin definitions for Heltec and TTGO-V1 LoRa-32 Boards with OLED SSD1306 I2C Display - Heltec LoRa 32(V2)
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+#define SOC_GPIO_PIN_HELTRK_OLED_RST    21
+#define SOC_GPIO_PIN_HELTRK_OLED_SDA    17
+#define SOC_GPIO_PIN_HELTRK_OLED_SCL    18
+#else
+// Hardware pin definitions for Heltec and TTGO-V1 LoRa-32 Boards with OLED SSD1306 I2C Display - Heltec LoRa 32(V3)
+#define SOC_GPIO_PIN_HELTRK_OLED_RST    16
+#define SOC_GPIO_PIN_HELTRK_OLED_SDA    4
+#define SOC_GPIO_PIN_HELTRK_OLED_SCL    15
+#endif
+
+
 // 1st I2C bus
 #define SOC_GPIO_PIN_HELTRK_SDA         6
 #define SOC_GPIO_PIN_HELTRK_SCL         7
 
 // LED
-#define SOC_GPIO_PIN_HELTRK_LED         18 /* white, active HIGH */
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+
+#define SOC_GPIO_PIN_HELTRK_LED         SOC_UNUSED_PIN /* 35 /*18 /* white, active HIGH */
+
+#else
+
+#define SOC_GPIO_PIN_HELTRK_LED         25 /* white, active HIGH */
+
+#endif
 
 // Misc.
 #define SOC_GPIO_PIN_HELTRK_VEXT_EN     3 /* V0.3 - active LOW, V0.5 - HIGH */
@@ -40,3 +74,19 @@
 // V0.5 only: 32768 Hz crystal
 #define SOC_GPIO_PIN_HELTRK_XP          15
 #define SOC_GPIO_PIN_HELTRK_XN          16
+
+
+//  Heltec specific
+#define DEFAULT_VREF            1100    // Default VREF use if no e-fuse calibration
+#define VBATT_SAMPLE            500     // Battery sample rate in ms
+#define VBATT_SMOOTH            50      // Number of averages in sample
+#define ADC_READ_STABILIZE      5       // in ms (delay from GPIO control and ADC connections times)
+#define LO_BATT_SLEEP_TIME      10*60*1000*1000     // How long when low batt to stay in sleep (us)
+
+#if (defined(CONFIG_IDF_TARGET_ESP32S3))
+#define VBATT_GPIO              37
+#define VOLTAGE_DIVIDER         4.90    // Lora has 390k/100k voltage divider so need to reverse that reduction via (390k+100k)/100k
+#else
+#define VBATT_GPIO              21      // Heltec GPIO to toggle VBatt read connection ... WARNING!!! This also connects VEXT to VCC=3.3v so be careful what is on header.  Also, take care NOT to have ADC read connection in OPEN DRAIN when GPIO goes HIGH
+#define VOLTAGE_DIVIDER         3.20    // Lora has 220k/100k voltage divider so need to reverse that reduction via (220k+100k)/100k on vbat GPIO37 or ADC1_1 (early revs were GPIO13 or ADC2_4 but do NOT use with WiFi.begin())
+#endif
