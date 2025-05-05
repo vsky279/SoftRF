2d16528918fc6170d57658ae4c1bdc10dcf8fac3
Lukáš Voborský <vsky279@users.noreply.github.com>
2025-05-06 00:18:49 +0200

    Remove not needed or even obstructing libraries for ESP32 build

--- software/firmware/source/libraries/EasyLink/src/smartrf_settings/smartrf_settings_predefined.h
+++
@@ -1,7 +0,0 @@
-#if defined(BOARD_CC1310_LAUNCHXL) || defined(BOARD_E70_XXXT14S2)
-#include "CC1310_LAUNCHXL/smartrf_settings_predefined.h"
-#elif defined(BOARD_CC1350_LAUNCHXL)
-#include "CC1350_LAUNCHXL/smartrf_settings_predefined.h"
-#elif defined(BOARD_CC1350STK)
-#include "CC1350STK/smartrf_settings_predefined.h"
-#endif
